8f565242776702e7e2d4669a918a44a2a8830986
Riley <rileyjhumphreys@gmail.com>
2025-01-13 15:29:25 +1000

    Refactored to use express.js

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-import http from 'http';
-import fs from 'fs';
-import path from 'path';
-import url from 'url';
+const express = require('express');
+const path = require('path');
+const app = express();
 
-http.createServer((req, res) => {
-    const query = url.parse(req.url);
-    let pathname = query.pathname;
+app.use(express.static(__dirname));
 
-    pathname = pathname === '/' ? '/index.html' : pathname;
-    if (!path.extname(pathname)) {
-        pathname += '.html';
-    }
-    const filename = "." + pathname;
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,  'index.html'));
+});
 
-    fs.readFile(filename, (err, data) => {
-        if (err) {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            return res.end("404 Not Found: ");
-        }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080);
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
